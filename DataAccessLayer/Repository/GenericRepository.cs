8c7ba370641582865251c66f7b55db52f8c94bcd
Liltaro <sametog03@gmail.com>
2023-05-02 10:44:16 +0300

    Add project files.

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		public void Delete(T t)
+		{
+			using var C = new Context();
+			C.Remove(t);
+			C.SaveChanges();
+		}
+
+		public List<T> GetList()
+		{
+			using var C = new Context();
+			return C.Set<T>().ToList();
+		}
+
+		public void Insert(T t)
+		{
+			using var C = new Context();
+			C.Add(t);
+			C.SaveChanges();
+		}
+
+		public void Update(T t)
+		{
+			using var C = new Context();
+			C.Update(t);
+			C.SaveChanges();
+		}
+	}
+}
---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Destination
+	{
+		[Key]
+		public int DestinationID { get; set; }
+        public string DestinationCity { get; set; }
+        public string DestinationTime { get; set; }
+        public double DestinationPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public bool Status { get; set; }
+    }
+}
