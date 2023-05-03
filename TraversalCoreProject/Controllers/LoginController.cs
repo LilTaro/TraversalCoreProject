1a1791a08c9787b2af6d008dcca8db159b85ddcf
Liltaro <sametog03@gmail.com>
2023-05-03 20:40:53 +0300

    Updates

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -1,21 +1,56 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Threading.Tasks;
+using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        [HttpGet]
+        private readonly UserManager<AppUser> _userManager;
+
+		public LoginController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
-        //[HttpPost]
-        //public IActionResult SignUp()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> SignUp(userRegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                UserName = model.Username,
+                Email = model.Mail
+            };
+            if (model.Password==model.confirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
         [HttpGet]
         public IActionResult SignIn()
         {
--- TraversalCoreProject/ViewComponents/Default/_Statistics.cs
+++ TraversalCoreProject/ViewComponents/Default/_Statistics.cs
@@ -12,7 +12,7 @@ namespace TraversalCoreProject.ViewComponents
             using var C = new Context();
             ViewBag.v1 = C.Destinations.Count();
             ViewBag.v2 = C.Guides.Count();
-            ViewBag.v3 = 350;
+            ViewBag.v3 = C.Users.Count();
             ViewBag.v4 = C.Comments.Count();
             return View();
         }
