e88cc5905bd9d2930519a3bb1131480076354ac9
Liltaro <sametog03@gmail.com>
2023-05-18 15:51:07 +0300

    Updates

--- TraversalCoreProject/ViewComponents/MemberDashboard/_MemberStatistics.cs
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_MemberStatistics.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace TraversalCoreProject.ViewComponents.MemberDashboard
 {
     public class _MemberStatistics:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        Context C = new Context();
+		public IViewComponentResult Invoke()
         {
+            ViewBag.reservation = C.Reservations.Count();
+            ViewBag.comment = C.Comments.Count();
+            ViewBag.guide = C.Guides.Count();
+            ViewBag.route = C.Destinations.Count();
+            ViewBag.announcement = C.Announcements.Count();
+            ViewBag.users = C.Users.Count();
+            ViewBag.score = "8.6";
             return View();
         }
     }
--- TraversalCoreProject/ViewComponents/MemberLayout/_MemberLayoutNavbar.cs
+++ TraversalCoreProject/ViewComponents/MemberLayout/_MemberLayoutNavbar.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace TraversalCoreProject.ViewComponents.MemberLayout
 {
-	public class _MemberLayoutNavbar:ViewComponent
+    public class _MemberLayoutNavbar:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly UserManager<AppUser> _userManager;
+
+        public _MemberLayoutNavbar(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
 		{
-			return View();
-		}
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.image = values.ImageUrl;
+            ViewBag.username = values.Name + " " + values.Surname;
+            return View();
+        }
 	}
 }
